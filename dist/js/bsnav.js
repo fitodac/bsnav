52f229a4cd305ae3d0ceda3649598ccc6eadd29e
fitodac <fitodac@gmail.com>
2018-05-17 15:57:45 -0300

    Dropdown functions

--- dist/js/bsnav.js
+++ dist/js/bsnav.js
@@ -9,12 +9,13 @@
 		initialize: function() {
 			this.event();
 			this.toggler();
-			// this.hoverDropdown();
+			this.dropdown();
 			// this.navbarSticky();
 			// this.navbarScrollspy();
 		},
 
 		event : function(){},
+
 
 		toggler : function(){
 			$('.navbar-toggler').each(function(){
@@ -25,6 +26,37 @@
 				});
 
 			});
+		},
+
+
+		dropdown: function(){
+
+			var _speed = 300;
+
+			$('.bsnav .nav-item.dropdown')
+			.on('mouseenter', function(){
+
+				var _el = $(this);
+
+				if( _el.hasClass('pop') || _el.hasClass('fade') || _el.hasClass('fadeup') || _el.hasClass('zoom') ){
+					_el.find('> .navbar-nav').addClass('in');
+				}else{
+					_el.find('> .navbar-nav').addClass('in').stop().slideDown(_speed);
+				}
+
+			})
+			.on('mouseleave', function(){
+
+				var _el = $(this);
+
+				if( _el.hasClass('pop') || _el.hasClass('fade') || _el.hasClass('fadeup') || _el.hasClass('zoom') ){
+					_el.find('> .navbar-nav').removeClass('in');
+				}else{
+					_el.find('> .navbar-nav').removeClass('in').stop().slideUp(_speed);
+				}
+
+			});
+
 		}
 
 	};
