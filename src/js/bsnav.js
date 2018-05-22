fa09c816491520902a4ee8e58ab0582f8f659845
fitodac <fitodac@gmail.com>
2018-05-22 00:49:31 -0300

    Megamenu

--- src/js/bsnav.js
+++ src/js/bsnav.js
@@ -29,8 +29,12 @@
 					_el.toggleClass('active');
 				});
 
+				$(window).resize(function(){ $('.navbar-toggler').removeClass('active'); });
+
 			});
 		},
+
+
 
 
 
@@ -48,18 +52,36 @@
 
 
 
+
+
 		mobileMenu: function(){
 
-			var _menu = $('<div/>', {
-						class: 'bsnav-mobile',
-						html: '<div class="bsnav-mobile-overlay"></div><div class="navbar"></div>'
-					});
+			// var _menu = $('<div/>', {
+			// 			class: 'bsnav-mobile',
+			// 			html: '<div class="bsnav-mobile-overlay"></div><div class="navbar"></div>'
+			// 		});
 
-			$('.bsnav .collapse').find('> .navbar-nav').each(function(){
-				_menu.find('.navbar').append( $(this).clone() );
+			$('.bsnav .navbar-mobile').each(function(){
+				if( $('.bsnav-mobile')[0] ){
+					var _m = $(this).clone();
+					// console.log(_m);
+					_m.appendTo('.bsnav-mobile .navbar');
+				}
 			});
 
-			_menu.appendTo('body');
+			
+			if( !$('.navbar-toggler').attr('data-target') ){
+				$('.navbar-toggler').on('click', function(){
+					$('.bsnav-mobile').toggleClass('in');
+				});
+			}
+
+			$('.bsnav-mobile .bsnav-mobile-overlay').on('click', function(){
+				$('.bsnav-mobile').removeClass('in');
+				$('.navbar-toggler').removeClass('active');
+			});
+
+			$(window).resize(function(){ $('.bsnav-mobile').removeClass('in'); });
 
 		},
 
