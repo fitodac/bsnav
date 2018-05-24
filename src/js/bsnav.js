85ed394403a31934d5b6052727aad7ecfb226260
fitodac <fitodac@gmail.com>
2018-05-24 01:46:46 -0300

    Sidebar nav levels

--- src/js/bsnav.js
+++ src/js/bsnav.js
@@ -87,15 +87,77 @@
 		},
 
 
+
+
+
 		sideMenu: function(){
 
 			if( $('.bsnav-sidebar')[0] ){
-				console.log('sidebar exists');
 
-				console.log( $('.bsnav-sidebar .navbar-nav').eq(0) );
+				var _sidebar = $('.bsnav-sidebar .navbar-nav').eq(0),
+						_num = 1;
+
+
+				_sidebar.find('.navbar-nav').each(function(){
+
+					var _id = 'navbar-nav-'+_num,
+							_index = $(this).parent().index();
+
+					$(this).parent().attr('data-child-navbar', _id);
+					$(this).prepend('<li class="nav-previous"></li>')
+					$(this).attr('id', _id);
+					_num++;
+
+				});
+
+
+				_sidebar.find('.navbar-nav').each(function(){
+					var _navbar = $(this).clone();
+					_navbar.find('.navbar-nav').detach();
+					_navbar.appendTo('.bsnav-sidebar .collapse');
+				});
+
+				_sidebar.find('.navbar-nav').remove();
+
+
+				$(document).find('.bsnav-sidebar .nav-link').on('click', function(e){
+					if( $(this).parent().attr('data-child-navbar') ){
+						e.preventDefault();
+
+						var _navbar = '#'+$(this).parent().attr('data-child-navbar'),
+								_parentNav = $(this).closest('.navbar-nav');
+
+						_parentNav.addClass('out');
+						$(_navbar).addClass('active');
+						$('.bsnav-sidebar .navbar-nav.prev').removeClass('prev');
+						setTimeout(function(){ 
+							_parentNav.removeClass('active').removeClass('out').addClass('prev');
+						}, 400);
+					}
+				});
+
+
+				$(document).find('.bsnav-sidebar .nav-previous').on('click', function(e){
+					$('.bsnav-sidebar .navbar-nav.active').addClass('out');
+					$('.bsnav-sidebar .navbar-nav.prev').addClass('active');
+					// $('.bsnav-sidebar .navbar-nav.prev').removeClass('prev');
+					setTimeout(function(){ 
+						$('.bsnav-sidebar .navbar-nav.out').removeClass('active').removeClass('out');
+						$('.bsnav-sidebar .navbar-nav.prev').addClass('prev');
+						// _parentNav.removeClass('active').removeClass('out').addClass('prev');
+					}, 400);
+				});
+
+
+				$(document).ready(function(){
+					_sidebar.addClass('active');
+				});
+
 			}
 
 		},
+
+
 
 	};
 
